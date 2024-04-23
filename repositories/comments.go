f6ec7eb1881932f2b7818203f5ec7104df7c1a24
日下陸矢 <rikuya.98@gmail.com>
2024-04-23 21:40:00 +0900

    　指定した記事に紐づくコメントの取得とコメントを新規作成する関数を追加

---
+++ repositories/comments.go
@@ -0,0 +1,52 @@
+package repositories
+
+import (
+	"database/sql"
+	"github.com/yourname/reponame/models"
+)
+
+func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
+	const sqlStr = `
+insert into comments (article_id, message, created_at)
+values (?, ?, now());
+`
+	var newComment models.Comment
+	result, err := db.Exec(sqlStr, comment.ArticleID, comment.Message)
+	if err != nil {
+		return newComment, err
+	}
+	id, _ := result.LastInsertId()
+	newComment.CommentID = int(id)
+	newComment.ArticleID, newComment.Message = comment.ArticleID, comment.Message
+
+	return newComment, nil
+}
+
+func SelectCommentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	const sqlStr = `
+select comment_id, article_id, message, created_at
+from comments
+where article_id = ?;
+`
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	getCommentList := make([]models.Comment, 0)
+
+	for rows.Next() {
+		var comment models.Comment
+		var createdTime sql.NullTime
+		err := rows.Scan(&comment.CommentID, &comment.ArticleID, &comment.Message, createdTime)
+		if createdTime.Valid {
+			comment.CreatedAt = createdTime.Time
+		}
+		if err != nil {
+			return []models.Comment{}, err
+		}
+		getCommentList = append(getCommentList, comment)
+	}
+	return getCommentList, nil
+}
