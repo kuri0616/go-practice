e4c71bbebc218c989bdecbc36aa51190edb14f8d
日下陸矢 <rikuya.98@gmail.com>
2024-04-24 21:26:23 +0900

    テーブルドリブンテストの作成

--- repositories/article_test.go
+++ repositories/article_test.go
@@ -21,31 +21,70 @@ func TestSelectArticle(t *testing.T) {
 	}
 	defer db.Close()
 
-	expected := models.Article{
-		ID:       1,
-		Title:    "Hello",
-		Content:  "Hello, world!",
-		UserName: "Alice",
-		NiceNum:  4,
-	}
-	got, err := repositories.SelectArticle(db, expected.ID)
-	if err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		testTitle string
+		expected  models.Article
+	}{
+		{
+			testTitle: "subtest1",
+			expected: models.Article{
+				ID:       1,
+				Title:    "Hello",
+				Content:  "Hello, world!",
+				UserName: "Alice",
+				NiceNum:  4,
+			},
+		}, {
+			testTitle: "subtest2",
+			expected: models.Article{
+				ID:       2,
+				Title:    "Goodbye",
+				Content:  "Goodbye, world!",
+				UserName: "Bob",
+				NiceNum:  0,
+			},
+		},
 	}
 
-	if got.ID != expected.ID {
-		t.Errorf("got %v, expected %v", got.ID, expected.ID)
+	for _, test := range tests {
+		t.Run(test.testTitle, func(t *testing.T) {
+			got, err := repositories.SelectArticle(db, test.expected.ID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got.ID != test.expected.ID {
+				t.Errorf("get %v, but want %v", got.ID, test.expected.ID)
+			}
+			if got.Title != test.expected.Title {
+				t.Errorf("get %v, but want %v", got.Title, test.expected.Title)
+			}
+			if got.Content != test.expected.Content {
+				t.Errorf("get %v, but want %v", got.Content, test.expected.Content)
+			}
+			if got.UserName != test.expected.UserName {
+				t.Errorf("get %v, but want %v", got.UserName, test.expected.UserName)
+			}
+		})
 	}
-	if got.Title != expected.Title {
-		t.Errorf("got %v, expected %v", got.Title, expected.Title)
-	}
-	if got.Content != expected.Content {
-		t.Errorf("got %v, expected %v", got.Content, expected.Content)
-	}
-	if got.UserName != expected.UserName {
-		t.Errorf("got %v, expected %v", got.UserName, expected.UserName)
-	}
-	if got.NiceNum != expected.NiceNum {
-		t.Errorf("got %v, expected %v", got.NiceNum, expected.NiceNum)
-	}
+
+	//got, err := repositories.SelectArticle(db, expected.ID)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//
+	//if got.ID != expected.ID {
+	//	t.Errorf("got %v, expected %v", got.ID, expected.ID)
+	//}
+	//if got.Title != expected.Title {
+	//	t.Errorf("got %v, expected %v", got.Title, expected.Title)
+	//}
+	//if got.Content != expected.Content {
+	//	t.Errorf("got %v, expected %v", got.Content, expected.Content)
+	//}
+	//if got.UserName != expected.UserName {
+	//	t.Errorf("got %v, expected %v", got.UserName, expected.UserName)
+	//}
+	//if got.NiceNum != expected.NiceNum {
+	//	t.Errorf("got %v, expected %v", got.NiceNum, expected.NiceNum)
+	//}
 }
