27537ff94c655f79127fbdfbe558b2b1b71e33e1
日下陸矢 <rikuya.98@gmail.com>
2024-04-25 20:40:26 +0900

    テストで使用するDBの前処理と後処理を追加

--- repositories/article_test.go
+++ repositories/article_test.go
@@ -1,54 +1,31 @@
 package repositories_test
 
 import (
-	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/yourname/reponame/models"
 	"github.com/yourname/reponame/repositories"
+	"github.com/yourname/reponame/repositories/testdata"
 	"testing"
 )
 
 func TestSelectArticle(t *testing.T) {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer db.Close()
-
 	tests := []struct {
 		testTitle string
 		expected  models.Article
 	}{
 		{
-			testTitle: "subtest1",
-			expected: models.Article{
-				ID:       1,
-				Title:    "Hello",
-				Content:  "Hello, world!",
-				UserName: "Alice",
-				NiceNum:  4,
-			},
-		}, {
-			testTitle: "subtest2",
-			expected: models.Article{
-				ID:       2,
-				Title:    "Goodbye",
-				Content:  "Goodbye, world!",
-				UserName: "Bob",
-				NiceNum:  0,
-			},
+			testTitle: "testTitle1",
+			expected:  testdata.ArticleTestData[0],
+		},
+		{
+			testTitle: "testTitle2",
+			expected:  testdata.ArticleTestData[1],
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.testTitle, func(t *testing.T) {
-			got, err := repositories.SelectArticle(db, test.expected.ID)
+			got, err := repositories.SelectArticle(testDB, test.expected.ID)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -66,25 +43,94 @@ func TestSelectArticle(t *testing.T) {
 			}
 		})
 	}
+}
 
-	//got, err := repositories.SelectArticle(db, expected.ID)
-	//if err != nil {
-	//	t.Fatal(err)
-	//}
-	//
-	//if got.ID != expected.ID {
-	//	t.Errorf("got %v, expected %v", got.ID, expected.ID)
-	//}
-	//if got.Title != expected.Title {
-	//	t.Errorf("got %v, expected %v", got.Title, expected.Title)
-	//}
-	//if got.Content != expected.Content {
-	//	t.Errorf("got %v, expected %v", got.Content, expected.Content)
-	//}
-	//if got.UserName != expected.UserName {
-	//	t.Errorf("got %v, expected %v", got.UserName, expected.UserName)
-	//}
-	//if got.NiceNum != expected.NiceNum {
-	//	t.Errorf("got %v, expected %v", got.NiceNum, expected.NiceNum)
-	//}
+func TestSelectArticleList(t *testing.T) {
+	expectedNum := len(testdata.ArticleTestData)
+	got, err := repositories.SelectArticleList(testDB, 1)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(got) != expectedNum {
+		t.Errorf("len(got) = %v, want %v", len(got), expectedNum)
+	}
+}
+
+func TestInsertArticle(t *testing.T) {
+	article := models.Article{
+		Title:    "yeah",
+		Content:  "yeah, world!",
+		UserName: "Charlie",
+	}
+
+	expectedArticleNum := 3
+	newArticle, err := repositories.InsertArticle(testDB, article)
+	if err != nil {
+		t.Error(err)
+	}
+	if newArticle.ID != expectedArticleNum {
+		t.Errorf("newArticle.ID = %v, want %v", newArticle.ID, expectedArticleNum)
+	}
+
+	t.Cleanup(func() {
+		const sqlStr = `
+		delete from articles
+		where article_id = ?;
+	`
+		_, err := testDB.Exec(sqlStr, newArticle.ID)
+	})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestUpdateNice(t *testing.T) {
+	expectedNiceNum := testdata.ArticleTestData[0].NiceNum + 1
+	articleID := testdata.ArticleTestData[0].ID
+	err := repositories.UpdateNice(testDB, articleID)
+	if err != nil {
+		t.Error(err)
+	}
+	if expectedNiceNum != testdata.ArticleTestData[0].NiceNum {
+		t.Errorf("expectedNiceNum = %v, want %v", expectedNiceNum, testdata.ArticleTestData[0].NiceNum)
+	}
+	t.Cleanup(func() {
+		beforeNiceNum := testdata.ArticleTestData[0].NiceNum - 1
+		const sqlStr = `
+		update articles
+		set nice = ?
+		where article_id = ?;
+	`
+		_, err := testDB.Exec(sqlStr, beforeNiceNum, articleID)
+
+		if err != nil {
+			t.Error(err)
+		}
+	})
+}
+
+func TestInsertComment(t *testing.T) {
+	comment := models.Comment{
+		ArticleID: 1,
+		Message:   "Hello, world!",
+	}
+	expectedCommentNum := 3
+
+	newComment, err := repositories.InsertComment(testDB, comment)
+	if err != nil {
+		t.Error(err)
+	}
+	if newComment.CommentID != expectedCommentNum {
+		t.Errorf("newComment.CommentID = %v, want %v", newComment.CommentID, expectedCommentNum)
+	}
+	t.Cleanup(func() {
+		const sqlStr = `
+		delete from comments
+		where comment_id = ?;
+	`
+		_, err := testDB.Exec(sqlStr, newComment.CommentID)
+		if err != nil {
+			t.Error(err)
+		}
+	})
 }
