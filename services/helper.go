74ea3f86549d1a2e9a8e2a8dd2771aae69f8ec01
日下陸矢 <rikuya.98@gmail.com>
2024-04-26 07:05:11 +0900

    サービス層に記事の投稿、取得のmethodを定義

--- services/helper.go
+++ services/helper.go
@@ -4,12 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"os"
 )
 
 var (
-	dbUser     = "docker"
-	dbPass     = "docker"
-	dbDatabase = "sampledb"
+	dbUser     = os.Getenv("DB_USER")
+	dbPass     = os.Getenv("DB_PASS")
+	dbDatabase = os.Getenv("DB_NAME")
 	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)%s?parseTime=true", dbUser, dbPass, dbDatabase)
 )
 
