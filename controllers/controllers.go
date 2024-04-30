08d46d6684e9fdc33b3c237344ac4a89c19ca808
日下陸矢 <rikuya.98@gmail.com>
2024-04-30 21:10:27 +0900

    　各層の依存関係を解消する

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,26 +1,29 @@
-package handlers
+package controllers
 
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/yourname/reponame/models"
 	"github.com/yourname/reponame/services"
-	"io"
 	"net/http"
 	"strconv"
 )
 
-func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
+type MyAppController struct {
+	service *services.MyAppService
 }
 
-func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+func NewMyAppController(s *services.MyAppService) *MyAppController {
+	return &MyAppController{service: s}
+}
+
+func (c *MyAppController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle models.Article
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "failed to decode json", http.StatusBadRequest)
 		return
 	}
-	resArticle, err := services.PostArticleSerrvice(reqArticle)
+	resArticle, err := c.service.PostArticleService(reqArticle)
 	if err != nil {
 		http.Error(w, "failed to insert article", http.StatusInternalServerError)
 		return
@@ -32,7 +35,7 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
 	if p, OK := queryMap["page"]; OK && len(p) > 0 {
 		page, err := strconv.Atoi(p[0])
@@ -40,7 +43,7 @@ func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
 			http.Error(w, "invalid page number", http.StatusBadRequest)
 			return
 		}
-		articleList, err := services.ArticleListHandler(page)
+		articleList, err := c.service.ArticleListHandler(page)
 		if err != nil {
 			http.Error(w, "failed to get article list", http.StatusInternalServerError)
 			return
@@ -52,13 +55,13 @@ func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func GetArticleHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) GetArticleHandler(w http.ResponseWriter, req *http.Request) {
 	articleId, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
 		http.Error(w, "invalid article id", http.StatusBadRequest)
 		return
 	}
-	article, err := services.GetArticleService(articleId)
+	article, err := c.service.GetArticleService(articleId)
 	if err != nil {
 		http.Error(w, "failed to get article", http.StatusInternalServerError)
 		return
@@ -70,13 +73,13 @@ func GetArticleHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func PostArticleNiceHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostArticleNiceHandler(w http.ResponseWriter, req *http.Request) {
 	articleId, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
 		http.Error(w, "invalid article id", http.StatusBadRequest)
 		return
 	}
-	article, err := services.PostNiceService(articleId)
+	article, err := c.service.PostNiceService(articleId)
 	if err != nil {
 		http.Error(w, "failed to update nice", http.StatusInternalServerError)
 		return
@@ -87,13 +90,13 @@ func PostArticleNiceHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
 	var reqComment models.Comment
 	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
 		http.Error(w, "failed to decode json", http.StatusBadRequest)
 		return
 	}
-	comment, err := services.PostCommentService(reqComment)
+	comment, err := c.service.PostCommentService(reqComment)
 	if err != nil {
 		http.Error(w, "failed to insert comment", http.StatusInternalServerError)
 		return
--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	"github.com/yourname/reponame/handlers"
+	"github.com/yourname/reponame/controllers"
+	"github.com/yourname/reponame/services"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	var (
+		dbUser     = os.Getenv("DB_USER")
+		dbPass     = os.Getenv("DB_PASS")
+		dbDatabase = os.Getenv("DB_NAME")
+		dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPass, dbDatabase)
+	)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ser := services.NewMyAppService(db)
+	con := controllers.NewMyAppController(ser)
 	r := mux.NewRouter()
 
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/list", handlers.GetArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.GetArticleHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostArticleNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", con.GetArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", con.GetArticleHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", con.PostArticleNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
